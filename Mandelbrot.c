b67e56cd2dcdb1c371917e6284f3d6aaae1e6e20
Luis <luis-scheuch@gmx.de>
2021-07-10 00:34:04 +0200

    add better positioning function

--- Mandelbrot.c
+++ Mandelbrot.c
@@ -7,19 +7,19 @@
 #include <time.h>
 #include <unistd.h>
 
-#define W 26000.0
+/*#define W 26000.0
 #define H 15000.0
 #define X_MAX -0.546
 #define X_MIN -0.572
 #define Y_MAX -0.55
-#define Y_MIN -0.565
-/*
+#define Y_MIN -0.565*/
+
 #define W 4000.0
 #define H 3000.0
-#define X_MAX 1.25
-#define X_MIN -2.25
-#define Y_MAX 1.25
-#define Y_MIN -1.25*/
+#define X_MAX_M 1.25
+#define X_MIN_M -2.25
+#define Y_MAX_M 1.25
+#define Y_MIN_M -1.25
 
 #define N_MAX 1000
 #define THREADS 160
@@ -33,22 +33,28 @@ double map(double x, double in_min, double in_max, double out_min, double out_ma
 
 long reku(double complex c, double complex z, long tiefe);
 void calc(uint32_t *data, long thread_nr, long threads);
-void draw_colour(uint32_t *data, int tiefe, double *bX, double *bY, double *mX, double *mY);
+void draw_colour(uint32_t *data, long tiefe, double *bX, double *bY, double *mX, double *mY);
+
+void calc_position(long x_middle, long y_middle, long zoom);
 
 DWORD WINAPI ThreadFunc(uint32_t* data);
 
-int thread_table[THREADS] = {0};
+long thread_table[THREADS] = {0};
+double X_MAX, X_MIN, Y_MAX, Y_MIN;
 clock_t start, end;
 double cpu_time_used;
 
-int main(void) {
+long main(void) {
 	
 	start = clock();
 	Beep(440,100);
 	//nice cc -c *.c;
 	
-	uint32_t *data = (uint32_t*) malloc(sizeof(uint32_t) * W * H); // Bilddaten
+	uint32_t *data = (uint32_t*) malloc(sizeof(uint32_t) * W * H); 	// Bilddaten
 	
+	
+	calc_position(-55, 0, 1000);									// (-100 to 100, -100 to 100, n) 
+	//printf("%d %d %d %d", X_MAX, X_MIN, Y_MAX, Y_MIN);
 	
 	printf("\nPainting background\n");
 	for (long i = 0; i < W * H; i++) {
@@ -61,7 +67,6 @@ int main(void) {
 	for (long i = 0; i < THREADS; i++) {
 		
 		HANDLE thread = CreateThread(NULL, 0, ThreadFunc, data, 0, NULL);
-		//usleep(1000);
 	}
 	
 	long not_finished = THREADS;
@@ -78,7 +83,6 @@ int main(void) {
 			printf("Threads finished: %d%%\n", (THREADS-not_finished)* 100/THREADS);
 			old_finished = not_finished;
 		}
-		//sleep(1);
 	}	
 	
 	printf("\nPainting graph\n");	
@@ -98,19 +102,16 @@ int main(void) {
 
 DWORD WINAPI ThreadFunc(uint32_t* data) {
 	long thread_nr = 0;
-	for(long i = 0; i < THREADS; i++) {
+	for(long i = 0; i < THREADS; i++) {  	// Extremely intelligent method to assign the tread_nr
 		if(*(thread_table + i) == 0) {
 			*(thread_table + i) += 1;
 			thread_nr = i;
 			break;
 		}
 	}
-	//printf("Thread Nr. %d started\n", thread_nr+1);
-	
 	calc(data, thread_nr, THREADS);
 	
-	//printf("\tThread Nr. %d finished\n", thread_nr+1);
-	*(thread_table + thread_nr) += 1;
+	*(thread_table + thread_nr) += 1; 		// marking as finished
 }
 
 void calc(uint32_t *data, long thread_nr, long threads) { //thread_nr 0 ... n - 1, threads n
@@ -122,7 +123,7 @@ void calc(uint32_t *data, long thread_nr, long threads) { //thread_nr 0 ... n - 
 	long tiefe = 0;
 	complex double c;
 	
-	for (long xw = W / threads * thread_nr; xw < W / threads * (thread_nr  + 1); xw++) {
+	for (long xw = W / threads * thread_nr; xw < W / threads * (thread_nr  + 1); xw++) {	// Splitting the picture in |THREADS| columns
 		for (long yh = 0; yh < H; yh++) {
 			
 			toMath(&xw, &yh, &mX, &mY);
@@ -171,12 +172,20 @@ long toPos(long x, long y) {
 	return((y * W) + x);
 }
 
-double map(double x, double in_min, double in_max, double out_min, double out_max) {		//credit to Arduino
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+double map(double x, double in_min, double in_max, double out_min, double out_max) {		// credit to Arduino
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;					// Map an input to a specific range
 }
 
-void draw_colour(uint32_t *data, int tiefe, double *bX, double *bY, double *mX, double *mY) {
+void draw_colour(uint32_t *data, long tiefe, double *bX, double *bY, double *mX, double *mY) {	// Write pixel_data to data
 	toBMP(bX, bY, mX, mY);
-	
 	*(data + toPos((long)round(*bX), (long)round(*bY))) = (long) map(tiefe, 0, N_MAX, 0, 16777215);
+}
+
+void calc_position(long x_middle, long y_middle, long zoom) {
+	
+	X_MIN = map(x_middle - 10000/zoom, -100, 100, X_MIN_M, X_MAX_M);
+	X_MAX = map(x_middle + 10000/zoom, -100, 100, X_MIN_M, X_MAX_M);
+	Y_MIN = map(y_middle + 10000/zoom, -100, 100, Y_MIN_M, Y_MAX_M);
+	Y_MAX = map(y_middle - 10000/zoom, -100, 100, Y_MIN_M, Y_MAX_M);
+	
 }
